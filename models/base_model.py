b5d7fd3d42133d8502bcd9c9ddcc05319ade655f
Alex Allen <325@holbertonschool.com>
2018-06-10 04:37:41 +0000

    using self.__dict__

--- models/base_model.py
+++ models/base_model.py
@@ -6,20 +6,14 @@ import datetime
 
 class BaseModel:
     """ This is the Base class. """
-    __nb_objects = 0
 
-    def __init__(self, created_at=None, updated_at=None, id=None):
+    def __init__(self, *args, **kwargs):
         """ Initialize the BaseModel class."""
-        if id is None:
-            self.id = str(uuid.uuid4())
-        else:
-            self.id = id
-
-        if created_at is None:
-            self.created_at = datetime.datetime.now()
-
-        if updated_at is None:
-            self.updated_at = datetime.datetime.now()
+#        if len(kwargs) != 0:
+#            to_dict(**kwargs)
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """Function returns string rep of the display function."""
@@ -31,19 +25,7 @@ class BaseModel:
 
     def to_dict(self):
         """Adds items to a_dict."""
-        a_dict = {}
-        key = ["id", "created_at", "updated_at", "__class__", "my_number", "name"]
-        for i in range(len(key)):
-            if key[i] == "id":
-                a_dict[key[i]] = self.id
-            if key[i] == "created_at":
-                a_dict[key[i]] = self.created_at.isoformat()
-            if key[i] == "updated_at":
-                a_dict[key[i]] = self.updated_at.isoformat()
-            if key[i] == "__class__":
-                a_dict[key[i]] = self.__class__.__name__
-            if key[i] == "my_number":
-                a_dict[key[i]] = 89
-            if key[i] == "name":
-                a_dict[key[i]] = "Holberton"
-        return a_dict
+        self.__dict__['created_at'] = self.created_at.isoformat()
+        self.__dict__['updated_at'] = self.updated_at.isoformat()
+        self.__dict__['__class__'] = self.__class__.__name__
+        return self.__dict__
