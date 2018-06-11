fa92228d37be6ed3b54a7769629bf97b8093e280
Alex Allen <325@holbertonschool.com>
2018-06-11 20:42:15 +0000

    new modules: show, create, emptyline

--- console.py
+++ console.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""
-The entry point for the command interpreter.
-"""
+""" The entry point for the command interpreter."""
 import cmd
+from models.base_model import BaseModel
+from models import storage
+import models
+import models.engine
+import argparse
 
 
 class HBNBCommand(cmd.Cmd):
@@ -16,6 +19,46 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, line):
         """Exits the program."""
         return True
+
+    def do_create(self, arg):
+        """ creates BaseModel instance, saves it to JSON, prints the id """
+        if arg == "":
+            print("** class name missing **")
+        elif arg == "BaseModel":
+            inst = BaseModel()
+            inst.save()
+            print(inst.id)
+        else:
+            print("** class doesn't exist **")
+
+    def do_show(self, line):
+        """ prints string rep of instance based on class and id """
+        all_objs = storage.all()
+        args = line.split()
+        try:
+            cls = args[0]
+        except:
+            print("** class name missing **")
+            return
+
+        try:
+            cid = args[1]
+        except:
+            print("** instance id missing **")
+            return
+
+        if cls == "BaseModel":
+            inst = BaseModel()
+            for obj_id in all_objs.keys():
+                if str("BaseModel." + cid) == obj_id:
+                    obj = all_objs[obj_id]
+                    print(obj)
+                    return
+            print("** no instance found **")
+
+    def emptyline(self):
+        """ overwrites Cmd.emptyline() """
+        pass
 
 if __name__ == '__main__':
     prompt = HBNBCommand()
